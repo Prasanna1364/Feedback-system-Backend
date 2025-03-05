e8510cbb0657155069f062fbad16984be2979c76
prasanna <2200031731@kluniversity.in>
2025-03-05 23:15:04 +0530

    Initial commit: Added frontend and backend project files

---
+++ faculty_course_service/src/main/java/klu/FacultyCourseServiceApplication.java
@@ -0,0 +1,13 @@
+package klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyCourseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyCourseServiceApplication.class, args);
+	}
+
+}
